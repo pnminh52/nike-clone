6ed5e44278444bc36071723abf7534c5bedc6bf3
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-08-30 11:56:50 +0700

    update homepage banner

--- src/components/user/homepage/ImageTable.jsx
+++ src/components/user/homepage/ImageTable.jsx
@@ -6,7 +6,7 @@ const ImageTable = () => {
      <div className='mx-auto max-w-screen-2xl px-6 sm:px-10'>
           <div className='hidden sm:block'>
             <div className='border-l border-r border-b rounded-b-3xl border-blue-600'>
-              <img className='w-full h-auto object-cover' src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_1286,c_limit/384ca7d3-eef0-435f-b91b-273f7238e252/nike-just-do-it.png" alt="" />
+              <img className='w-full h-auto object-cover' src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1247,c_limit/fe626a12-7dfb-4f60-a67d-2808ad1fe3e8/nike-just-do-it-nike-com-jp.jpg" alt="" />
             <div className="flex justify-center">
                 <div className="text-center py-6 space-y-2 max-w-xl">
                   <h3 className="nike-title w-full text-center justify-center bg-gradient-to-r from-[#D397FA] to-black bg-clip-text text-transparent" data-qa="title">
@@ -27,7 +27,7 @@ const ImageTable = () => {
           </div>
           <div className='block sm:hidden'>
             <div className=''>
-            <img className='w-full h-auto object-cover' src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_528,c_limit/20271b1c-8fae-446a-9794-cb061daeaa8e/nike-just-do-it.jpg" alt="" />
+            <img className='w-full h-auto object-cover' src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_436,c_limit/c7a850e7-6d45-43f9-9a1c-858845391134/nike-just-do-it-nike-com-jp.jpg" alt="" />
             <div className="">
                        <div className=" py-6 space-y-2 max-w-xl">
                          <h3 className="nike-title-for-mobile w-full  bg-gradient-to-r from-[#D397FA] to-black bg-clip-text text-transparent" data-qa="title">
